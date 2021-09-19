e141f727774d035576f84974e819aab4e4af111f
jliocsar <juliocesarmfo@gmail.com>
2021-09-19 10:12:14 -0300

    Add missing MigrationError codes

--- src/index.ts
+++ src/index.ts
@@ -260,6 +260,39 @@ export const MigrationError = {
    * The datasource provider `{provider}` specified in your schema does not match the one specified in the migration_lock.toml. You will encounter errors when you try to apply migrations generated for a different provider. Please archive your current migration directory at a different location and start a new migration history with `prisma migrate dev`.
    */
   DatasourceProviderDontMatchMigrationLock: 'P3014',
+  /**
+   * Could not find the migration file at `{migration_file_path}`. Please delete the directory or restore the migration file.
+   */
+  MissingMigrationFile: 'P3015',
+  /**
+   * The fallback method for database resets failed, meaning Migrate could not clean up the database entirely. Original error: `{error_code}`
+   * `{inner_error}`
+   */
+  CouldNotCleanupDatabase: 'P3016',
+  /**
+   * The migration `{migration_name}` could not be found. Please make sure that the migration exists, and that you included the whole name of the directory. (example: `"20201207184859_initial_migration"`)
+   */
+  MigrationNotFound: 'P3017',
+  /**
+   * A migration failed to apply. New migrations can not be applied before the error is recovered from. Read more about how to resolve migration issues in a production database: https://pris.ly/d/migrate-resolve
+   *
+   * Migration name: `{migration_name}`
+   *
+   * Database error code: `{database_error_code}`
+   *
+   * Database error:
+   * `{database_error}`
+   */
+  FailedToApplyMigration: 'P3018',
+  /**
+   * The datasource provider `{provider}` specified in your schema does not match the one specified in the migration_lock.toml, `{expected_provider}`. Please remove your current migration directory and start a new migration history with prisma migrate dev. Read more: https://pris.ly/d/migrate-provider-switch
+   */
+  DatasourceProvidersDontMatch: 'P3019',
+  /**
+   * The automatic creation of shadow databases is disabled on Azure SQL. Please set up a shadow database using the `shadowDatabaseUrl` datasource attribute.
+   * Read the docs page for more details: https://pris.ly/d/migrate-shadow
+   */
+  ShadowDatabasesAutomaticCreationIsDisabled: 'P3020',
 } as const
 
 export const PrismaError = {
